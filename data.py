df2a28c7f12ceb09ebf2cd105d60e28a3e7ebc45
solz9 <125681221+solz9@users.noreply.github.com>
2023-05-12 08:51:59 +0700

    Update data.py

--- data.py
+++ data.py
@@ -1,26 +1,19 @@
+# Khang Trương
 import streamlit as st
-from deta import Deta
-import cv2
 from PIL import Image
-# import database
-# deta = 'c0qy5dgedq2_SNHHnXR1972LBCNH5fcgizZzuaAT4XtA'
-# Data to be written to Deta Base
-with st.form("form"):
-    name = st.text_input("Your name")
-    img = st.camera_input('Chụp ảnh')
-    submitted = st.form_submit_button("Store in database")
+import deta
 
+# Step 4: Capture the image using st.camera_input()
+image = st.camera_input()
 
-# Connect to Deta Base with your Data Key
-deta = Deta(st.secrets["data_key"])
+# Step 5: Convert the image data to a suitable format (e.g., bytes)
+image_bytes = image.read()
 
-# Create a new database "example-db"
-# If you need a new database, just use another name.
-db = deta.Base("key_reg")
+# Step 6: Initialize the Deta base
+db = deta.Base('YOUR_PROJECT_KEY')
 
-# If the user clicked the submit button,
-# write the data from the form to the database.
-# You can store any data you want here. Just modify that dictionary below (the entries between the {}).
-if submitted:
-    st.write(img.read())
-    db.put({"name": name, "pic": cv2_img})
+# Step 7: Save the image data to the Deta base
+db.put(image_bytes, 'image.jpg')
+
+# Display a success message
+st.success('Image saved to Deta database!')
