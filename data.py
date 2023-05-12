faa841882a3d461cbda4b51d5dc81e2a9d2a8fd3
solz9 <125681221+solz9@users.noreply.github.com>
2023-05-12 07:14:03 +0700

    Update data.py

--- data.py
+++ data.py
@@ -27,4 +27,4 @@ if submitted:
 # This reads all items from the database and displays them to your app.
 # db_content is a list of dictionaries. You can do everything you want with it.
 db_content = db.fetch().items
-st.write(db_content)
+st.write(db_content['name'])
