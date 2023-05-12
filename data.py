0f48c68eefa69192f79b178b23224d261dee5cf2
solz9 <125681221+solz9@users.noreply.github.com>
2023-05-12 10:42:36 +0700

    Update data.py

--- data.py
+++ data.py
@@ -8,7 +8,7 @@ with st.form("form"):
     submitted = st.form_submit_button("Store in database")
 
 deta = Deta(st.secrets["data_key"])
-db = deta.Base("project_face_reg")'
+db = deta.Base("project_face_reg")
 if submitted:
     if img_file_buffer is not None:
 
