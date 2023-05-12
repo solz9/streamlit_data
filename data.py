98256a50a159f30a741fc20fd6f7336fd5fbe30c
solz9 <125681221+solz9@users.noreply.github.com>
2023-05-12 10:32:45 +0700

    Update data.py

--- data.py
+++ data.py
@@ -2,7 +2,7 @@ import streamlit as st
 from PIL import Image
 import numpy as np
 from deta import Deta
-img_file_buffer = st.camera_input("Take a picture")
+# img_file_buffer = st.camera_input("Take a picture")
 deta = Deta(st.secrets["data_key"])
 db = deta.Base("key_reg")
 # if img_file_buffer is not None:
