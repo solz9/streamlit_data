26c1803b5b6af3d3f476a30009b85d4e4316245f
solz9 <125681221+solz9@users.noreply.github.com>
2023-05-12 08:53:18 +0700

    Update data.py

--- data.py
+++ data.py
@@ -4,7 +4,7 @@ from PIL import Image
 import deta
 
 # Step 4: Capture the image using st.camera_input()
-image = st.camera_input()
+image = st.camera_input('take picture')
 
 # Step 5: Convert the image data to a suitable format (e.g., bytes)
 image_bytes = image.read()
