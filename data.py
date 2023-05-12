27270259319f2b8e37a3d9c1ec7b4a08be7bfed4
solz9 <125681221+solz9@users.noreply.github.com>
2023-05-12 12:20:58 +0700

    Update data.py

--- data.py
+++ data.py
@@ -1,21 +1,33 @@
+# import streamlit as st
+# from PIL import Image
+# import numpy as np
+# from deta import Deta
+# from pydrive.auth import GoogleAuth
+# from pydrive.drive import GoogleDrive
+# with st.form("form"):
+#     name = st.text_input("Your name")
+#     img_file_buffer = st.camera_input('Chụp ảnh')
+#     submitted = st.form_submit_button("Store in database")
+
+# deta = Deta(st.secrets["data_key"])
+# db = deta.Base("project_face_reg")
+# if submitted:
+#     if img_file_buffer is not None:
+
+#         img = Image.open(img_file_buffer)
+
+
+#         img_array = np.array(img)
+
+#         db.put({'key': name, "pic": img_array.tolist()})
+/////////////////////////////////////////////////////////
 import streamlit as st
-from PIL import Image
-import numpy as np
-from deta import Deta
-with st.form("form"):
-    name = st.text_input("Your name")
-    img_file_buffer = st.camera_input('Chụp ảnh')
-    submitted = st.form_submit_button("Store in database")
 
-deta = Deta(st.secrets["data_key"])
-db = deta.Base("project_face_reg")
-if submitted:
-    if img_file_buffer is not None:
+img_file_buffer = st.camera_input("Take a picture")
 
-        img = Image.open(img_file_buffer)
-
-
-        img_array = np.array(img)
-
-        db.put({'key': name, "pic": img_array.tolist()})
-
+if img_file_buffer is not None:
+    # To read image file buffer as bytes:
+    bytes_data = img_file_buffer.getvalue()
+    # Check the type of bytes_data:
+    # Should output: <class 'bytes'>
+    st.write(type(bytes_data))
