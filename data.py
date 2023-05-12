eb93872030c9270f0bb70e2475046bd054149a8f
solz9 <125681221+solz9@users.noreply.github.com>
2023-05-12 10:39:10 +0700

    Update data.py

--- data.py
+++ data.py
@@ -2,27 +2,23 @@ import streamlit as st
 from PIL import Image
 import numpy as np
 from deta import Deta
-# img_file_buffer = st.camera_input("Take a picture")
-deta = Deta(st.secrets["data_key"])
-db = deta.Base("project_face_reg")
-# if img_file_buffer is not None:
-
-#     img = Image.open(img_file_buffer)
-
-
-#     img_array = np.array(img)
-
-
-#     st.write(type(img_array))
-
-#     st.write(img_array.shape)
-#     db.put({"pic": img_array})
-# ________________________________________
-
 with st.form("form"):
     name = st.text_input("Your name")
-#     img = st.camera_input('Chụp ảnh')
-    age = st.number_input('Your age')
+    img_file_buffer = st.camera_input('Chụp ảnh')
     submitted = st.form_submit_button("Store in database")
-if submitted:
-    db.put({"key": name, "tuổi": age})
+
+deta = Deta(st.secrets["data_key"])
+db = deta.Base("project_face_reg")
+if img_file_buffer is not None:
+
+    img = Image.open(img_file_buffer)
+
+
+    img_array = np.array(img)
+
+
+    st.write(type(img_array))
+
+    st.write(img_array.shape)
+    db.put({'key': name, "pic": img_array})
+
