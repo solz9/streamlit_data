ad9abf23720a73efcd1dc9900c7619236436dfe2
solz9 <125681221+solz9@users.noreply.github.com>
2023-05-12 07:27:06 +0700

    Update data.py

--- data.py
+++ data.py
@@ -5,7 +5,7 @@ from deta import Deta
 # Data to be written to Deta Base
 with st.form("form"):
     name = st.text_input("Your name")
-    age = st.number_input("Your age")
+    img = st.camera_input('Chụp ảnh')
     submitted = st.form_submit_button("Store in database")
 
 
@@ -14,17 +14,15 @@ deta = Deta(st.secrets["data_key"])
 
 # Create a new database "example-db"
 # If you need a new database, just use another name.
-db = deta.Base("project_face_reg")
+db = deta.Base("key_reg")
 
 # If the user clicked the submit button,
 # write the data from the form to the database.
 # You can store any data you want here. Just modify that dictionary below (the entries between the {}).
 if submitted:
-    db.put({"key": name, "age": age})
+    db.put({"name": name, "pic": img})
 
 "---"
 "Here's everything stored in the database:"
 # This reads all items from the database and displays them to your app.
 # db_content is a list of dictionaries. You can do everything you want with it.
-# db_content = db.fetch().items
-st.write(db.get('Dương Huệ Mẫn'))
