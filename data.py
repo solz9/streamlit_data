0694ccb78eac2190bc1188a9e6603a1dd0e645fb
solz9 <125681221+solz9@users.noreply.github.com>
2023-05-15 00:42:54 +0700

    Update data.py

--- data.py
+++ data.py
@@ -6,21 +6,23 @@ DETA_KEY = "c0ub5ly8vj3_8JHKeMTN4yEGj6wJGNj16nNww4mRwFJE" # Secret key to connec
 deta = Deta(DETA_KEY) # Initialize deta object with a project key
 
 drive = deta.Drive("face_registration") # Connecting to the Deta drive
-name = st.text_input('nhập tên')
-# Here i'm taking the input from `st.file_uploader`, same principle can be  applied.
-uploaded_files = st.camera_input("Choose photos to upload")
-st.set_option('deprecation.showfileUploaderEncoding', False) # Enabling the automatic file decoder
+with st.form("my_form"):
+    name = st.text_input('nhập tên')
+    # Here i'm taking the input from `st.file_uploader`, same principle can be  applied.
+    uploaded_files = st.camera_input("Choose photos to upload")
+    st.set_option('deprecation.showfileUploaderEncoding', False) # Enabling the automatic file decoder
+submitted = st.form_submit_button("Submit")
+if submitted:
+    file = io.BytesIO(b'this is a byte string')
+    pic_names = [] # Later used for deleting the local files after being uploaded
+    for uploaded_file in uploaded_files: # Iterating over each file uploaded
+        file = io.BytesIO(uploaded_file)
+        file = file.read() # Read the data
+    #     image_result = open(uploaded_file.name, 'wb') # creates a writable image and later we can write the decoded result
+    #     image_result.write(file) # Saves the file with the name uploaded_file.name to the root path('./')
+    #     pic_names.append(uploaded_file.name) # Append the name of image to the list
+    #     image_result.close() # Close the file pointer
 
-file = io.BytesIO(b'this is a byte string')
-pic_names = [] # Later used for deleting the local files after being uploaded
-for uploaded_file in uploaded_files: # Iterating over each file uploaded
-    file = io.BytesIO(uploaded_file)
-    file = file.read() # Read the data
-#     image_result = open(uploaded_file.name, 'wb') # creates a writable image and later we can write the decoded result
-#     image_result.write(file) # Saves the file with the name uploaded_file.name to the root path('./')
-#     pic_names.append(uploaded_file.name) # Append the name of image to the list
-#     image_result.close() # Close the file pointer
-if uploaded_files:
     for i in range(len(pic_names)): # Iterating over each file name
         names = name # Getting the name of current file
         path ='./'+pic_names[i] # Creating path string which is basically ["./image.jpg"]
