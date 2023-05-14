15168ac3b8461eb83080fb0e181f8089b96a05d3
solz9 <125681221+solz9@users.noreply.github.com>
2023-05-15 00:43:40 +0700

    Update data.py

--- data.py
+++ data.py
@@ -11,21 +11,21 @@ with st.form("my_form"):
     # Here i'm taking the input from `st.file_uploader`, same principle can be  applied.
     uploaded_files = st.camera_input("Choose photos to upload")
     st.set_option('deprecation.showfileUploaderEncoding', False) # Enabling the automatic file decoder
-submitted = st.form_submit_button("Submit")
-if submitted:
-    file = io.BytesIO(b'this is a byte string')
-    pic_names = [] # Later used for deleting the local files after being uploaded
-    for uploaded_file in uploaded_files: # Iterating over each file uploaded
-        file = io.BytesIO(uploaded_file)
-        file = file.read() # Read the data
-    #     image_result = open(uploaded_file.name, 'wb') # creates a writable image and later we can write the decoded result
-    #     image_result.write(file) # Saves the file with the name uploaded_file.name to the root path('./')
-    #     pic_names.append(uploaded_file.name) # Append the name of image to the list
-    #     image_result.close() # Close the file pointer
+    submitted = st.form_submit_button("Submit")
+    if submitted:
+        file = io.BytesIO(b'this is a byte string')
+        pic_names = [] # Later used for deleting the local files after being uploaded
+        for uploaded_file in uploaded_files: # Iterating over each file uploaded
+            file = io.BytesIO(uploaded_file)
+            file = file.read() # Read the data
+        #     image_result = open(uploaded_file.name, 'wb') # creates a writable image and later we can write the decoded result
+        #     image_result.write(file) # Saves the file with the name uploaded_file.name to the root path('./')
+        #     pic_names.append(uploaded_file.name) # Append the name of image to the list
+        #     image_result.close() # Close the file pointer
 
-    for i in range(len(pic_names)): # Iterating over each file name
-        names = name # Getting the name of current file
-        path ='./'+pic_names[i] # Creating path string which is basically ["./image.jpg"]
-        drive.put(names, path=path) # so, we have our file name and path, so uploading images to the drive
-        os.remove(pic_names[i]) # Finally deleting it from root folder
-    st.success('Thanks for uploading!') # Success message
+        for i in range(len(pic_names)): # Iterating over each file name
+            names = name # Getting the name of current file
+            path ='./'+pic_names[i] # Creating path string which is basically ["./image.jpg"]
+            drive.put(names, path=path) # so, we have our file name and path, so uploading images to the drive
+            os.remove(pic_names[i]) # Finally deleting it from root folder
+        st.success('Thanks for uploading!') # Success message
