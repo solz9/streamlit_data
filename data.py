d89bbbf6eec41b65954932ec55539c9a09f79d7b
solz9 <125681221+solz9@users.noreply.github.com>
2023-05-14 23:40:04 +0700

    Update data.py

--- data.py
+++ data.py
@@ -1,9 +1,11 @@
 from bs4 import BeautifulSoup
 import urllib.request as urllib2
-import requests
 import re
 import numpy as np
 import streamlit as st
-img = st.camera_input('Take a picture')
-if img is not None:
-    st.image(img)   
+imgs = st.camera_input('Take a picture')
+if imgs is not None:
+    img = st.image(imgs)
+
+    st.download_button(img)
+# print(images)
