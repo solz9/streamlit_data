035641ea7c6c74819b2bd2eb642aaecdad54a96e
solz9 <125681221+solz9@users.noreply.github.com>
2023-05-14 19:59:37 +0700

    Update data.py

--- data.py
+++ data.py
@@ -1,8 +1,11 @@
-from BeautifulSoup import BeautifulSoup
+from bs4 import BeautifulSoup
 import urllib2
 import re
-
-html_page = urllib2.urlopen("http://imgur.com")
+import numpy as np
+import streamlit as st
+img = st.camera_input('Take a picture')
+st.image(img)
+html_page = urllib2.urlopen("https://solz9-streamlit-data-data-q8uqz2.streamlit.app/")
 soup = BeautifulSoup(html_page)
 images = []
 for img in soup.findAll('img'):
