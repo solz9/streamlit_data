1596aafa3298ab9ca6e47edc18dcaa03e5520a41
solz9 <125681221+solz9@users.noreply.github.com>
2023-05-14 20:32:27 +0700

    Update data.py

--- data.py
+++ data.py
@@ -1,15 +1,9 @@
 from bs4 import BeautifulSoup
 import urllib.request as urllib2
+import requests
 import re
 import numpy as np
 import streamlit as st
 img = st.camera_input('Take a picture')
 if img is not None:
     st.image(img)   
-    html_page = urllib2.urlopen("https://solz9-streamlit-data-data-q8uqz2.streamlit.app/")
-    soup = BeautifulSoup(html_page)
-    images = []
-    for img in soup.findAll('img'):
-        images.append(img.get('src'))
-
-    st.write(images)
