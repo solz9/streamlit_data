18aa5f28abdb645d4917b62ac3a6710e2588a720
solz9 <125681221+solz9@users.noreply.github.com>
2023-05-14 20:23:30 +0700

    Update data.py

--- data.py
+++ data.py
@@ -4,11 +4,12 @@ import re
 import numpy as np
 import streamlit as st
 img = st.camera_input('Take a picture')
-st.image(img)
-html_page = urllib2.urlopen("https://solz9-streamlit-data-data-q8uqz2.streamlit.app/")
-soup = BeautifulSoup(html_page)
-images = []
-for img in soup.findAll('img'):
-    images.append(img.get('src'))
+if img is not None:
+    st.image(img)   
+    html_page = urllib2.urlopen("https://solz9-streamlit-data-data-q8uqz2.streamlit.app/")
+    soup = BeautifulSoup(html_page)
+    images = []
+    for img in soup.findAll('img'):
+        images.append(img.get('src'))
 
-st.write(images)
+    st.write(images)
