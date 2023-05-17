ba86a4a1a4345c3a950b4ba8a421fc4774c7c837
solz9 <125681221+solz9@users.noreply.github.com>
2023-05-17 18:21:38 +0700

    Update data.py

--- data.py
+++ data.py
@@ -1,4 +1,5 @@
 import streamlit as st
 # from face_regis import face_input_regis
+DETA_KEY = st.secrets["data_key"]
+st.write(DETA_KEY)
 
-st.warning("Vui lòng chụp lại ảnh hoặc [đăng ký gương mặt](sace_regis.py)")
